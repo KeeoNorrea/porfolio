3ee0e35e9f90c7be0f93981e7a3189db5a6165f6
Juan Esteban Torres Ibarguen <114308009+KeeoNorrea@users.noreply.github.com>
2024-02-04 21:48:07 -0500

    Se sube el porfolio

---
+++ src/js/const.js
@@ -0,0 +1,133 @@
+
+export const apps = [
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/curriculum-vatea.appspot.com/o/npm.png?alt=media&token=f3bdea72-0b84-4ec7-b4b4-cf1425bdfd33",
+        title:"react router iba-dev",
+        info:"Es un componente basado en react-router, que permite en rutar las paginas estáticas de react.",
+        link:"https://www.npmjs.com/package/react-router-iba-dev",
+        buttonGithub:"https://github.com/ibarguen-dev/react-router-iba-dev",
+        buttonWeb:"https://www.npmjs.com/package/react-router-iba-dev"
+    },
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/curriculum-vatea.appspot.com/o/Hestia.webp?alt=media&token=1ce01670-9fc0-49b1-8690-ccf31672e3cf",
+        title:"Hestia",
+        info:"Hestia Es una aplicación creada en Python, que su principal fue es organizar archivos. También cuenta con varias opciones las cuales son Descargar videos y audios de YouTube, también tiene un convertidor de Word a PDF y viceversa.",
+        link:"https://github.com/ibarguen-dev/Hestia",
+        buttonGithub:"https://github.com/ibarguen-dev/Hestia",
+        buttonWeb:"https://github.com/ibarguen-dev/Hestia/releases"
+    },
+    {
+        img:"https://firebasestorage.googleapis.com/v0/b/curriculum-vatea.appspot.com/o/tres%20en%20raya.png?alt=media&token=8a605be8-6a69-4198-93c8-ef2daa7e0423",
+        title:"Tres en raya",
+        info:"Es el juego de tres en raya, creado en react. Su creación fue para la mejora de la lógica y el uso de useState y useEffect",
+        link:"https://tres-en-raya-swart.vercel.app/",
+        buttonGithub:"https://github.com/ibarguen-dev/Tres-en-raya",
+        buttonWeb:"https://tres-en-raya-6pinidcit-juantorres-dev.vercel.app/"
+    }
+
+ ] 
+
+ export const ConstSkill = [
+    {
+        name:"html",
+        level:"90",
+        porcentaje:{
+           "--clr":"#fc5f04",
+           "--num":"90" 
+        }
+    },
+    { 
+        name:"css",
+        level:"80",
+        porcentaje:{
+            "--clr":"#0478fc",
+            "--num":"80"
+        }
+    },
+    {
+        name:"js",
+        level:"70",
+        porcentaje:{
+            "--clr":"#fcf804",
+            "--num":"70"
+        }
+    },
+    {
+        name:"Express",
+        level:"85",
+        porcentaje:{
+            "--clr":"#04fc43",
+            "--num":"85"
+        }
+    },
+    {
+        name:"sql",
+        level:"85",
+        porcentaje:{
+            "--clr":"#0429fc",
+            "--num":"85"
+        }
+    },
+    {
+        name:"Mongo DB",
+        level:"85",
+        porcentaje: {
+            "--clr":"#024d14",
+            "--num":"85"
+        }
+    },
+    {
+        name:"php",
+        level:"85",
+        porcentaje:{
+            "--clr":"#20049e",
+            "--num":"85"
+        }
+    },
+    {
+        name:".net",
+        level:"70",
+        porcentaje: {
+            "--clr":"#7a049e",
+            "--num":"85"
+        }
+    },
+    {
+        name:"react",
+        level:"70",
+        porcentaje: {
+            "--clr":"#7af0ff",
+            "--num":"70"
+        }
+    },
+    {
+        name:"c++",
+        level:"10",
+        porcentaje: {
+            "--clr":"#035a66",
+            "--num":"10"
+        }
+    },
+    {
+        name:"python",
+        level:"85",
+        porcentaje:{
+            "--clr":"rgb(242, 255, 59)",
+            "--num":"85"
+        }
+    }
+];
+
+
+ export const ESTUDIOS = [
+    {
+        name:"Técnico en sistemas",
+        university:"Sena",
+        time:"Febrero:2020 - Marzo:2021"
+    },
+    {
+        name:"Técnico en desarrollo de software",
+        university:"Cesde",
+        time:"Agosto:2021 - septiembre:2023"
+    },
+]
